98b93663e7e8aac7e31a5b12543629299c7d4c86
Joshua Hedstrom <jhhedstrom@gmail.com>
2021-09-23 21:45:38 -0400

    commit tree e7c1b818cf0dd8430ce8506037837205645063e4

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+const hq = 42
+const feet = 264
+
+function distanceFromHqInBlocks(pickUp) {
+    if (pickUp > hq) {
+        return pickUp - hq;
+    }
+    else if (pickUp < hq) {
+        return hq - pickUp;
+    }
+}
+
+function distanceFromHqInFeet(pickUp) {
+    return distanceFromHqInBlocks(pickUp) * feet;
+}
+
+
+function distanceTravelledInFeet(north, south) {
+    if (north > south) {
+        return ((north - south) * feet);
+    }
+    else if (north < south) {
+        return ((south - north) * feet);
+    }
+    else {
+        return feet
+    }
+}
+
+
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    if (distanceInFeet <= 400) {
+        return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <=2000) {
+        return 0.02 * (distanceInFeet - 400);
+    } else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
